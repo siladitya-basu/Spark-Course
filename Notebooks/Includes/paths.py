6c440f683e9e2859875e24f2525270de8bd038f2
siladitya-basu <wrickbasu@gmail.com>
2022-06-13 14:19:44 +0530

    nothing commit

--- Notebooks/Includes/paths.py
+++
@@ -1,6 +0,0 @@
-# from turtle import home
-
-data_path = '/home/siladitya/Documents/Spark/Spark-Course/Data/'
-raw_data_path = '/home/siladitya/Documents/Spark/Spark-Course/Data/imdb/'
-sample_data_path = '/home/siladitya/Documents/Spark/Spark-Course/Data/sample-data/'
-spark_warehouse_path = '/home/siladitya/Documents/Spark/Spark-Course/Data/spark-warehouse/'
